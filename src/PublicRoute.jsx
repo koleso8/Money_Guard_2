d34fc6ec46b16ff34e316820ffceee1db04c5480
IVAN KOLESNYCHENKO <168295877+koleso8@users.noreply.github.com>
2024-09-05 15:30:38 +0300

    redux auth (#1)

--- src/PublicRoute.jsx
+++ src/PublicRoute.jsx
@@ -1,10 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-// import { selectIsLoggedIn } from '../redux/auth/selectors';
+import { selectIsLoggedIn } from './redux/auth/selectors';
 
 export const PublicRoute = ({ component: Component, redirectTo = '/' }) => {
-  const isLoggedIn = true;
-  //useSelector(selectIsLoggedIn);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
