0c0a9312cd59b539ea7aad875e01fe8ab31e85eb
IVAN KOLESNYCHENKO <168295877+koleso8@users.noreply.github.com>
2024-09-06 00:13:26 +0300

    Redux kolesnychenko (#9)
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Update index.html
    
    * debug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,51 +1,51 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import {
-	persistStore,
-	persistReducer,
-	FLUSH,
-	REHYDRATE,
-	PAUSE,
-	PERSIST,
-	PURGE,
-	REGISTER,
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { currencyReducer } from './currency/slice';
 import { authReducer } from './auth/slice';
 
 const persistConfig = {
-	key: 'root',
-	version: 1,
-	storage,
-	whiteList: ['user'],
+  key: 'root',
+  version: 1,
+  storage,
+  whiteList: ['user'],
 };
 
 const currencyPersistConfig = {
-	key: 'currency',
-	storage,
-	whitelist: ['currencies', 'lastRequestTime'],
+  key: 'currency',
+  storage,
+  whitelist: ['currencies', 'lastRequestTime'],
 };
 
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const currencyPersistedReducer = persistReducer(
-	currencyPersistConfig,
-	currencyReducer
+  currencyPersistConfig,
+  currencyReducer
 );
 
 export const store = configureStore({
-	reducer: {
-		auth: persistedReducer,
-		currency: currencyPersistedReducer,
-		// transactions: transactionsReducer,
-		// editing: editingReducer,
-	},
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-			},
-		}),
+  reducer: {
+    auth: persistedReducer,
+    currency: currencyPersistedReducer,
+    // transactions: transactionsReducer,
+    // editing: editingReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
