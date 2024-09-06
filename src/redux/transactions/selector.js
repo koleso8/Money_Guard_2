646e6c5331ec9defd6ec34a6edfe71671ea11f62
koleso8 <dev.koleso@gmail.com>
2024-09-06 13:05:20 +0300

    add Transactions thunks

--- src/redux/transactions/selector.js
+++ src/redux/transactions/selector.js
@@ -1,8 +1,8 @@
-export const selectIsError = state => state.transactions.error;
-
-export const selectIsLoading = state => console.log(state);
-
-// state.transactions.loading;
-export const selectContacts = state => state.transactions.items;
-
-// export const selectCurrent = state => state.editing.current;
+export const selectTransactions = state => state.transactions.items;
+export const selectPeriodTransactions = state =>
+  state.transactions.periodTransactions;
+export const selectIsLoading = state => state.transactions.loading;
+export const selectError = state => state.transactions.error;
+export const selectCurrentTransaction = state =>
+  state.transactions.currentTransaction;
+export const selectCategories = state => state.transactions.categories;
