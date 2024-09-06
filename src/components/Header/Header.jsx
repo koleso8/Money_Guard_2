08e1ca78cc7cd4e9bd9a792e483955e75a4adede
gramatchi <nik98.champion@gmail.com>
2024-09-06 17:09:32 +0200

    add onClick

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,36 @@ import s from './Header.module.css';
 import clsx from 'clsx';
 
 const Header = () => {
+  const handleLogout = () => {
+    console.log("Logout clicked");
+  };
+
+  const handleExit = () => {
+    console.log("Exit clicked");
+  };
+
+  const redirectToHome = () => {
+    console.log("Home clicked");
+  };
+
   return (
     <div className={clsx(s.container)}>
-      <div className={clsx(s.logo)}>
-        <svg className={clsx(s.logosvg)} width="18" height="18">
+      <div className={clsx(s.logo)} onClick={redirectToHome}>
+        <svg className={clsx(s.logosvg)}>
           <use href="./src/images/icons.svg#logo"></use>
         </svg>
         <p className={clsx(s.title)}>Money Guard</p>
       </div>
       <div className={clsx(s.logout)}>
         <p className={clsx(s.username)}>Name</p>
-        <svg className={clsx(s.logoutSvg)} width="18" height="18">
-          <use href="./src/images/icons.svg#logout"></use>
-        </svg>
-        <p className={clsx(s.exit)}>Exit</p> 
+        <button className={clsx(s.logoutButton)} onClick={handleLogout}>
+          <svg width="18" height="18">
+            <use href="./src/images/icons.svg#logout"></use>
+          </svg>
+        </button>
+        <button className={clsx(s.exitButton)} onClick={handleExit}>
+          Exit
+        </button>
       </div>
     </div>
   );
