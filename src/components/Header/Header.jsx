f6612f527102c4026bb9f6ad552849daaac680c7
gramatchi <nik98.champion@gmail.com>
2024-09-06 16:50:21 +0200

    add header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import s from './Header.module.css';
+import clsx from 'clsx';
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className={clsx(s.container)}>
+      <div className={clsx(s.logo)}>
+        <svg className={clsx(s.logosvg)} width="18" height="18">
+          <use href="./src/images/icons.svg#logo"></use>
+        </svg>
+        <p className={clsx(s.title)}>Money Guard</p>
+      </div>
+      <div className={clsx(s.logout)}>
+        <p className={clsx(s.username)}>Name</p>
+        <svg className={clsx(s.logoutSvg)} width="18" height="18">
+          <use href="./src/images/icons.svg#logout"></use>
+        </svg>
+        <p className={clsx(s.exit)}>Exit</p> 
+      </div>
+    </div>
+  );
 };
 
 export default Header;
