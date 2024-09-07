5031c4f7114063c0b8674af5f1bfd7a16b4292ed
Yevhenii98765 <156183790+Yevhenii98765@users.noreply.github.com>
2024-09-07 10:16:36 +0300

    Onyshchenko/registration page  (#21)
    * RegistrationForm / logic bones
    
    * Login Form / logic bones
    
    * LoginForm/Basis of stylization
    
    * Update RegistrationForm.jsx
    
    * RegistrationFrom/Basis of stylization
    
    ---------
    
    Co-authored-by: Kenediii <156183790+Kenediii@users.noreply.github.com>

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,13 +1,15 @@
-import { Field, Form, Formik } from "formik";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { registerThunk } from "../../redux/auth/operations";
+import { Field, Form, Formik } from 'formik';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { registerThunk } from '../../redux/auth/operations';
+import s from './RegistrationForm.module.css';
+
 export const RegistrationForm = () => {
   const dispath = useDispatch();
   const initialValues = {
-    username: "",
-    email: "",
-    password: "",
+    username: '',
+    email: '',
+    password: '',
   };
 
   const handleSubmit = (values, options) => {
@@ -17,40 +19,61 @@ export const RegistrationForm = () => {
   };
 
   return (
-    <div>
-      <div>
-        <div>
-          <h1>Money Guard</h1>
-        </div>
-        <div>
-          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-            <Form>
-              <div>
-                <label>
-                  <span>Name</span>
-                </label>
-                <Field name="username" placeholder="name" />
-              </div>
-              <div>
-                <label>
-                  <span>Email</span>
-                </label>
-                <Field name="email" type="email" placeholder="email" />
-              </div>
-              <div className="form-control">
-                <label>
-                  <span>Password</span>
-                </label>
-                <Field name="password" type="password" placeholder="password" />
-              </div>
-              <div>
-                <button>Rgister</button>
-                <label>
-                  <Link to="/login">Log in</Link>
-                </label>
-              </div>
-            </Form>
-          </Formik>
+    <div className={s.fon}>
+      <div className={s.iner}>
+        <div className={s.wrap}>
+          <div className={s.wrap_logo}>
+            <svg className={s.logo} width={36} height={36}>
+              <use href="./src/images/icons.svg#icon--Money-Guard-2"></use>
+            </svg>
+            <h1 className={s.title}>Money Guard</h1>
+          </div>
+          <div>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+              <Form className={s.form}>
+                <div className={s.wrap_input}>
+                  <svg width={24} height={24} className={s.svg_form_name}>
+                    <use href="./src/images/icons.svg#icon-userFrom"></use>
+                  </svg>
+                  <Field
+                    name="username"
+                    placeholder="Name"
+                    className={s.input}
+                  />
+                </div>
+                <div className={s.wrap_input}>
+                  <svg width={20} height={16} className={s.svg_form_first}>
+                    <use href="./src/images/icons.svg#icon-emailForm"></use>
+                  </svg>
+                  <Field
+                    name="email"
+                    type="email"
+                    placeholder="E-mail"
+                    className={s.input}
+                  />
+                </div>
+                <div className={s.wrap_input}>
+                  <svg width={16} height={21} className={s.svg_form_second}>
+                    <use href="./src/images/icons.svg#icon-passwordForm"></use>
+                  </svg>
+                  <Field
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    className={s.input}
+                  />
+                </div>
+                <div className={s.wrap_button}>
+                  <button className={s.btn_first}>Rgister</button>
+                  <label>
+                    <Link to="/login" className={s.btn_second}>
+                      Log in
+                    </Link>
+                  </label>
+                </div>
+              </Form>
+            </Formik>
+          </div>
         </div>
       </div>
     </div>
