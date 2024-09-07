98cba534c160e029598973bcee0c2da5ffb054a6
Gleb Yasynskyi <91141358+GYaskey@users.noreply.github.com>
2024-09-07 12:54:33 +0300

    Gleb transaction list (#26)
    * add
    
    * Delete vite.svg
    
    * Update index.css
    
    * redux auth (#1)
    
    * Redux kolesnychenko (#2)
    
    * redux auth
    
    * add react-hot-tost
    
    * Onyshchenko/ registration page  (#3)
    
    * RegistrationForm / logic bones
    
    * Login Form / logic bones
    
    ---------
    
    Co-authored-by: Kenediii <156183790+Kenediii@users.noreply.github.com>
    
    * add currency, balance, navigation, laout, dashboard not completed (#4)
    
    * Redux kolesnychenko (#5)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * Redux kolesnychenko (#6)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * fix (#7)
    
    * Redux kolesnychenko (#8)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Redux kolesnychenko (#9)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Update index.html
    
    * debug
    
    * Separate-folder-created (#10)
    
    * Redux kolesnychenko (#11)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Update index.html
    
    * debug
    
    * Update README.md
    
    * Update README.md
    
    * Redux kolesnychenko (#12)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Update index.html
    
    * debug
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Redux kolesnychenko (#13)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Update index.html
    
    * debug
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Redux kolesnychenko (#14)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Update index.html
    
    * debug
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Redux kolesnychenko (#15)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Update index.html
    
    * debug
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Redux kolesnychenko (#16)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Update index.html
    
    * debug
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add backdrop
    
    * Redux kolesnychenko (#17)
    
    * redux auth
    
    * add react-hot-tost
    
    * add  prettier
    
    * add vercel config
    
    * Update slice.js
    
    * Update index.html
    
    * Update index.html
    
    * debug
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add backdrop
    
    * add Transactions thunks (#18)
    
    * markup + styles + basic logic
    
    ---------
    
    Co-authored-by: koleso8 <dev.koleso@gmail.com>
    Co-authored-by: IVAN KOLESNYCHENKO <168295877+koleso8@users.noreply.github.com>
    Co-authored-by: Yevhenii98765 <156183790+Yevhenii98765@users.noreply.github.com>
    Co-authored-by: Kenediii <156183790+Kenediii@users.noreply.github.com>
    Co-authored-by: Dima Hutsol <dimaaa291997@gmail.com>
    Co-authored-by: Oksanium <164672938+Oksanium@users.noreply.github.com>

---
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -0,0 +1,40 @@
+import clsx from "clsx";
+import s from "./TransactionsList.module.css";
+// import { useSelector } from "react-redux";
+// import { selectTransactions } from "../../redux/transactions/selector";
+import TransactionsItem from "../TransactionsItem/TransactionsItem";
+import transactionData from "../../helpers/placeholderTransactions.json";
+import { FiPlus } from "react-icons/fi";
+
+const TransactionsList = () => {
+  //   const allTransactions = useSelector(selectTransactions);
+
+  const allTransactions = transactionData;
+
+  return (
+    <div className={clsx(s.transactionListBox)}>
+      <table className={clsx(s.table)}>
+        <thead>
+          <tr className={clsx(s.tableHeader)}>
+            <th className={clsx(s.date)}>Date</th>
+            <th className={clsx(s.type)}>Type</th>
+            <th className={clsx(s.category)}>Category</th>
+            <th className={clsx(s.comment)}>Comment</th>
+            <th className={clsx(s.sum)}>Sum</th>
+            <th className={clsx(s.actions)}></th>
+          </tr>
+        </thead>
+        <tbody className={clsx(s.tableBody)}>
+          {allTransactions.map((item) => (
+            <TransactionsItem key={item.id} {...item} />
+          ))}
+        </tbody>
+      </table>
+      <button type="button" className={clsx(s.addTrnBtn)}>
+        <FiPlus size={30} color="fff" />
+      </button>
+    </div>
+  );
+};
+
+export default TransactionsList;
