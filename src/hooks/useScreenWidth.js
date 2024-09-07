d12dac03dd5c5e0e3153f186cfc20c9ced5e567f
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-09-07 17:25:21 +0100

    complete balance and add adaptive

--- src/hooks/useScreenWidth.js
+++ src/hooks/useScreenWidth.js
@@ -1,9 +1,9 @@
 import { useMediaQuery } from 'react-responsive';
 
 export const useScreenWidth = () => {
-	const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' });
-	const isMediumScreen = useMediaQuery({ query: '(max-width: 1440px)' });
-	const isLargeScreen = useMediaQuery({ query: '(min-width: 1441px)' });
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 767px)' });
+  const isMediumScreen = useMediaQuery({ query: '(max-width: 1439px)' });
+  const isLargeScreen = useMediaQuery({ query: '(min-width: 1441px)' });
 
-	return { isSmallScreen, isMediumScreen, isLargeScreen };
+  return { isSmallScreen, isMediumScreen, isLargeScreen };
 };
