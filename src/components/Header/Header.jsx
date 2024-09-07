b49d673bcbcb517e88657f5999a50b91a7a794fd
gramatchi <nik98.champion@gmail.com>
2024-09-07 09:24:41 +0200

    Header/gramatchi (#20)
    * add header styles
    
    * add onClick
    
    * add navlink and username display
    
    * modal start
    
    ---------
    
    Co-authored-by: IVAN KOLESNYCHENKO <168295877+koleso8@users.noreply.github.com>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,90 @@
-import s from './Header.module.css';
-import clsx from 'clsx';
+
+import { useDispatch, useSelector } from "react-redux";
+import s from "./Header.module.css";
+import clsx from "clsx";
+import { selectUser } from "../../redux/auth/selectors";
+import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import ModalBackdrop from "../ModalBackdrop/ModalBackdrop";
+import { logoutThunk } from "../../redux/auth/operations";
 
 const Header = () => {
+  const user = useSelector(selectUser);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const dispatch = useDispatch();
+
   const handleLogout = () => {
-    console.log("Logout clicked");
+    setIsModalOpen(true);
   };
 
-  const handleExit = () => {
-    console.log("Exit clicked");
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
-  const redirectToHome = () => {
-    console.log("Home clicked");
-  };
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    };
+    if (isModalOpen) {
+      document.addEventListener("keydown", handleKeyDown);
+    }
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isModalOpen]);
 
   return (
     <div className={clsx(s.container)}>
-      <div className={clsx(s.logo)} onClick={redirectToHome}>
-        <svg className={clsx(s.logosvg)}>
-          <use href="./src/images/icons.svg#logo"></use>
-        </svg>
-        <p className={clsx(s.title)}>Money Guard</p>
-      </div>
+      <NavLink to="/">
+        <div className={clsx(s.logo)}>
+          <svg className={clsx(s.logosvg)}>
+            <use href="./src/images/icons.svg#logo"></use>
+          </svg>
+          <p className={clsx(s.title)}>Money Guard</p>
+        </div>
+      </NavLink>
       <div className={clsx(s.logout)}>
-        <p className={clsx(s.username)}>Name</p>
+        <p className={clsx(s.username)}>{user.username}</p>
+
         <button className={clsx(s.logoutButton)} onClick={handleLogout}>
           <svg width="18" height="18">
             <use href="./src/images/icons.svg#logout"></use>
           </svg>
         </button>
-        <button className={clsx(s.exitButton)} onClick={handleExit}>
+
+        <button className={clsx(s.exitButton)} onClick={handleLogout}>
           Exit
         </button>
       </div>
+
+      {isModalOpen && (
+        <ModalBackdrop>
+          <div className={s.back}>
+            <div className={clsx(s.logo)}>
+              <svg className={clsx(s.logosvg)}>
+                <use href="./src/images/icons.svg#logo"></use>
+              </svg>
+              <p className={clsx(s.title)}>Money Guard</p>
+            </div>
+            <div className={s.modalContent}>
+              <p className={s.modalText}>Are you sure you want to log out?</p>
+              <button
+                className={s.logoutBtn}
+                onClick={() => {
+                  dispatch(logoutThunk());
+                }}
+              >
+                L O G O U T
+              </button>
+              <button className={s.cancelBtn} onClick={closeModal}>
+                C A N C E L
+              </button>
+            </div>
+          </div>
+        </ModalBackdrop>
+      )}
     </div>
   );
 };
