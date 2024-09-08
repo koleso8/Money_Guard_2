3dd376db580a43247983133c12272b90543cb661
Dima Hutsol <dimaaa291997@gmail.com>
2024-09-08 07:12:38 +0100

    Dima/balance (#33)
    * complete balance and add adaptive
    
    * fix images
    
    * fix desktop media width

--- src/hooks/useScreenWidth.js
+++ src/hooks/useScreenWidth.js
@@ -2,7 +2,7 @@ import { useMediaQuery } from 'react-responsive';
 
 export const useScreenWidth = () => {
   const isSmallScreen = useMediaQuery({ query: '(max-width: 767px)' });
-  const isMediumScreen = useMediaQuery({ query: '(max-width: 1439px)' });
+  const isMediumScreen = useMediaQuery({ query: '(max-width: 1279px)' });
   const isLargeScreen = useMediaQuery({ query: '(min-width: 1441px)' });
 
   return { isSmallScreen, isMediumScreen, isLargeScreen };
