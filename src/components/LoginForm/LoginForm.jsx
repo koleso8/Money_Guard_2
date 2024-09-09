c45ef2609d20649e6289ee7feb983e1e6b3ceac0
Kenediii <156183790+Kenediii@users.noreply.github.com>
2024-09-09 18:20:19 +0300

    Fix style

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -5,12 +5,25 @@ import { loginThunk } from '../../redux/auth/operations';
 import s from './LoginForm.module.css';
 import Icon from '../Icon/Icon';
 import { Toaster } from 'react-hot-toast';
+import * as Yup from 'yup';
+
 export const LoginForm = () => {
   const dispath = useDispatch();
   const initialValues = {
     email: '',
     password: '',
   };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .min(3, 'To short text')
+      .max(15, 'To long text')
+      .required('Enter Text'),
+    password: Yup.string()
+      .min(6, 'To short text')
+      .max(12, 'To long text')
+      .required('Enter Text'),
+  });
 
   const handleSubmit = (values, options) => {
     console.log(values);
@@ -27,8 +40,12 @@ export const LoginForm = () => {
             <Icon width={36} height={36} name="icon--Money-Guard-2" />
             <h1 className={s.title}>Money Guard</h1>
           </div>
-          <div>
-            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+          <div className={s.iner_form}>
+            <Formik
+              initialValues={initialValues}
+              onSubmit={handleSubmit}
+              validationSchema={validationSchema}
+            >
               <Form className={s.form}>
                 <div className={s.wrap_input}>
                   <Icon
@@ -43,6 +60,7 @@ export const LoginForm = () => {
                     placeholder="E-mail"
                     className={s.input}
                   />
+                  <div className={s.line}></div>
                 </div>
                 <div className={s.wrap_input}>
                   <Icon
@@ -57,6 +75,7 @@ export const LoginForm = () => {
                     placeholder="Password"
                     className={s.input}
                   />
+                  <div className={s.line}></div>
                 </div>
                 <div className={s.wrap_button}>
                   <button className={s.btn_first}>Log in</button>
