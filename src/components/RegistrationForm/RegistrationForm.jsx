2b4d135bebd10518e825e60e5f6ec19390732a6d
Kenediii <156183790+Kenediii@users.noreply.github.com>
2024-09-10 13:50:19 +0300

    Fix / Yup / Bar / Style

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -6,26 +6,9 @@ import s from './RegistrationForm.module.css';
 import PasswordStrengthBar from 'react-password-strength-bar';
 import Icon from '../Icon/Icon';
 import { Toaster } from 'react-hot-toast';
-import * as Yup from 'yup';
+import validationSchema from '../../helpers/registrationSchema';
 
 export const RegistrationForm = () => {
-  const validationSchema = Yup.object().shape({
-    username: Yup.string()
-      .min(3, 'To short text')
-      .max(15, 'To long text')
-      .required('Enter Text'),
-    email: Yup.string()
-      .min(3, 'To short text')
-      .max(15, 'To long text')
-      .required('Enter Text'),
-    password: Yup.string()
-      .min(6, 'To short text')
-      .max(12, 'To long text')
-      .required('Enter Text'),
-    confirmPass: Yup.string()
-      .oneOf([Yup.ref('password')], 'passwords don`t match')
-      .required('Enter Text'),
-  });
   const dispath = useDispatch();
   const initialValues = {
     username: '',
@@ -138,6 +121,7 @@ export const RegistrationForm = () => {
                       />
                       <div className={s.line}></div>
                       <PasswordStrengthBar
+                        className={s.bar}
                         password={values.confirmPass}
                         scoreWords={['', '', '', '', '']}
                         shortScoreWord={['']}
--- src/helpers/registrationSchema.js
+++ src/helpers/registrationSchema.js
@@ -1,15 +1,21 @@
 import * as Yup from 'yup';
 
-export const registrationSchema = Yup.object().shape({
-  name: Yup.string()
-    .trim()
-    .min(3, 'Must be at least 3 characters')
-    .max(20, 'Must be less  than 20 characters')
-    .required('Name is required'),
-  email: Yup.string().trim().email().required('Email is required'),
+const validationSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, 'Name must be at least 3 characters')
+    .max(25, 'A maximum of 25 characters is allowed')
+    .required('Mandatory field'),
+  email: Yup.string()
+    .min(3, 'E-mail must be at least 3 characters')
+    .max(25, 'A maximum of 25 characters is allowed')
+    .required('Mandatory field'),
   password: Yup.string()
-    .trim()
-    .min(3, 'Must be at least 3 characters')
-    .max(20, 'Must be less  than 20 characters')
-    .required('Password is required'),
+    .min(6, 'Password must be at least 6 characters')
+    .max(12, 'A maximum of 12 characters is allowed')
+    .required('Mandatory field'),
+  confirmPass: Yup.string()
+    .oneOf([Yup.ref('password')], 'Passwords don`t match')
+    .required('Mandatory field'),
 });
+
+export default validationSchema;
