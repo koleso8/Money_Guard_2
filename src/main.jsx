90523ff79ee0df4a01fd71a7476f2afabc6d534a
koleso8 <dev.koleso@gmail.com>
2024-09-10 17:54:38 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { persistor, store } from './redux/store.js';
+
+import App from './App.jsx';
+
 import './styles/reset.css';
 import './styles/common.css';
 import './styles/variables.css';
