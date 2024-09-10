600576c8b16f082ace89496178e8a038e868049c
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-09-10 14:41:01 +0100

    rework modal

--- src/components/ModalBackdrop/ModalBackdrop.jsx
+++ src/components/ModalBackdrop/ModalBackdrop.jsx
@@ -2,20 +2,22 @@ import Modal from 'react-modal';
 import clsx from 'clsx';
 import s from './ModalBackdrop.module.css';
 import Icon from '../Icon/Icon';
-import { useSelector } from 'react-redux';
-import { selectHeaderHeight } from '../../redux/modal/selector';
+import { useSelector, useDispatch } from 'react-redux';
+import { closeModal } from '../../redux/modal/slice';
+import {
+  selectHeaderHeight,
+  selectActiveModal,
+} from '../../redux/modal/selector';
 import { useScreenWidth } from '../../hooks/useScreenWidth';
 
 Modal.setAppElement('#root');
 
-const ModalBackdrop = ({
-  children,
-  isOpen,
-  closeModal,
-  noCloseButton = false,
-}) => {
+const ModalBackdrop = ({ children, modalType, noCloseButton = false }) => {
   const { isSmallScreen } = useScreenWidth();
   const headerHeight = useSelector(selectHeaderHeight);
+  const activeModal = useSelector(selectActiveModal);
+  const dispatch = useDispatch();
+
   const modalMarginFromTop = isSmallScreen ? headerHeight : 0;
 
   const customStyles = {
@@ -24,16 +26,20 @@ const ModalBackdrop = ({
     },
   };
 
+  const closeModalHandler = () => {
+    dispatch(closeModal());
+  };
+
   return (
     <Modal
-      isOpen={isOpen}
-      onRequestClose={closeModal}
+      isOpen={activeModal === modalType}
+      onRequestClose={closeModalHandler}
       className={s.modal}
       overlayClassName={clsx(s.modalOverlay)}
       style={customStyles}
     >
       {!noCloseButton && !isSmallScreen && (
-        <button className={clsx(s.iconButton)} onClick={closeModal}>
+        <button className={clsx(s.iconButton)} onClick={closeModalHandler}>
           <Icon
             name="icon-close"
             height="16px"
--- src/redux/modal/slice.js
+++ src/redux/modal/slice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   headerHeight: 0,
+  activeModal: null, // Додаємо новий стейт для контролю активної модалки
 };
 
 const slice = createSlice({
@@ -11,8 +12,14 @@ const slice = createSlice({
     setHeaderHeight: (state, action) => {
       state.headerHeight = action.payload;
     },
+    openModal: (state, action) => {
+      state.activeModal = action.payload; // Зберігаємо тип активної модалки
+    },
+    closeModal: state => {
+      state.activeModal = null; // Закриваємо будь-яку активну модалку
+    },
   },
 });
 
 export const modalReducer = slice.reducer;
-export const { setHeaderHeight } = slice.actions;
+export const { setHeaderHeight, openModal, closeModal } = slice.actions;
