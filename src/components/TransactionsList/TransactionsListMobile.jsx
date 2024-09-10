a70889964301ec235d3327789b39c2676accefa3
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-09-10 15:45:37 +0100

    fix table and modal

--- src/components/TransactionsList/TransactionsListMobile.jsx
+++ src/components/TransactionsList/TransactionsListMobile.jsx
@@ -1,39 +1,42 @@
-import clsx from "clsx";
-import s from "./TransactionsList.module.css";
-import { useSelector } from "react-redux";
-import { selectTransactions } from "../../redux/transactions/selector";
-import { FiPlus } from "react-icons/fi";
-import { useState } from "react";
-import ModalBackdrop from "../ModalBackdrop/ModalBackdrop";
-import AddTransactionForm from "../AddTransactionForm/AddTransactionForm";
-import EditTransactionForm from "../EditTransactionForm/EditTransactionForm";
-import TransactionsItemMobile from "../TransactionsItem/TransactionsItemMobile";
+import clsx from 'clsx';
+import s from './TransactionsList.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectTransactions } from '../../redux/transactions/selector';
+import { FiPlus } from 'react-icons/fi';
+import { openModal, closeModal } from '../../redux/modal/slice';
+import ModalBackdrop from '../ModalBackdrop/ModalBackdrop';
+import AddTransactionForm from '../AddTransactionForm/AddTransactionForm';
+import EditTransactionForm from '../EditTransactionForm/EditTransactionForm';
+import TransactionsItemMobile from '../TransactionsItem/TransactionsItemMobile';
+import { useState } from 'react';
 
 const TransactionsListMobile = () => {
   const allTransactions = useSelector(selectTransactions)
     .slice()
     .sort((a, b) => new Date(b.transactionDate) - new Date(a.transactionDate));
 
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const dispatch = useDispatch();
+
   const [editedItem, setEditedItem] = useState(null);
 
-  const openAddModal = () => setIsAddModalOpen(true);
-  const closeAddModal = () => {
-    setIsAddModalOpen(false);
+  const openAddModal = () => {
     setEditedItem(null);
+    dispatch(openModal('addTransaction'));
+  };
+
+  const openEditModal = item => {
+    setEditedItem(item);
+    dispatch(openModal('editTransaction'));
   };
 
   return (
     <div>
       <ul>
-        {allTransactions.map((item) => (
+        {allTransactions.map(item => (
           <TransactionsItemMobile
             key={item.id}
             {...item}
-            setEditedItem={() => {
-              setEditedItem(item);
-              openAddModal();
-            }}
+            setEditedItem={() => openEditModal(item)}
           />
         ))}
       </ul>
@@ -44,11 +47,16 @@ const TransactionsListMobile = () => {
       >
         <FiPlus size={30} color="fff" />
       </button>
-      <ModalBackdrop isOpen={isAddModalOpen} closeModal={closeAddModal}>
-        {editedItem ? (
-          <EditTransactionForm closeModal={closeAddModal} {...editedItem} />
-        ) : (
-          <AddTransactionForm closeModal={closeAddModal} />
+
+      <ModalBackdrop modalType="addTransaction" noCloseButton={false}>
+        <AddTransactionForm closeModal={() => dispatch(closeModal())} />
+      </ModalBackdrop>
+      <ModalBackdrop modalType="editTransaction" noCloseButton={false}>
+        {editedItem && (
+          <EditTransactionForm
+            closeModal={() => dispatch(closeModal())}
+            {...editedItem}
+          />
         )}
       </ModalBackdrop>
     </div>
